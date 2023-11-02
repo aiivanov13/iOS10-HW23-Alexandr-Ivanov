dc6ad178274e5203e61dbe19ee21184fbb55ef1b
aivanov <alex130896@gmail.com>
2023-11-02 20:14:08 +0400

    add uikit screen

--- iOS10-HW23-Alexandr-Ivanov/View/Models/SearchModel.swift
+++ iOS10-HW23-Alexandr-Ivanov/View/Models/SearchModel.swift
@@ -23,7 +23,15 @@ extension CategoryItem {
             CategoryItem(title: "Пространственное звучание", imageName: "station5"),
             CategoryItem(title: "Хип-хоп на русском", imageName: "station6"),
             CategoryItem(title: "Хип-хоп", imageName: "station7"),
-            CategoryItem(title: "Рок", imageName: "station8")
+            CategoryItem(title: "Рок", imageName: "station8"),
+            CategoryItem(title: "Рок н ролл", imageName: "station1"),
+            CategoryItem(title: "Диско", imageName: "station2"),
+            CategoryItem(title: "Панк", imageName: "station3"),
+            CategoryItem(title: "Кантри", imageName: "station4"),
+            CategoryItem(title: "Расслабляющая музыка", imageName: "station5"),
+            CategoryItem(title: "Бодрое утро", imageName: "station6"),
+            CategoryItem(title: "Уютный вечер", imageName: "station7"),
+            CategoryItem(title: "Вечеринка", imageName: "station8")
         ]
     }
 }
